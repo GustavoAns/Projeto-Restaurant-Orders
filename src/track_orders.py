dd5b49e04c8ca4a2cd91331c2e8c5e10e7f94734
GustavoAns <gugasato9@live.com>
2022-07-19 17:27:15 -0300

    Add req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,47 @@
+from src.analyze_log import best_seller, how_many_request, never_requested, day_never_used
+
 class TrackOrders:
+    def __init__(self):
+        self._data = []
+    
     # aqui deve expor a quantidade de estoque
     def __len__(self):
-        pass
+        return len(self._data)
 
     def add_new_order(self, customer, order, day):
-        pass
+        return self._data.append({'cliente': customer, 'pedido': order, 'dia': day})
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return best_seller(self._data, customer)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        return never_requested(self._data, customer)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return day_never_used(self._data, customer)
 
     def get_busiest_day(self):
-        pass
+        allDays = {}
+        busiest_day = self._data[0]['dia']
+
+        for sale in self._data:
+            if sale['dia'] not in allDays:
+                allDays[sale['dia']] = 1
+            else:
+                allDays[sale['dia']] += 1
+            if allDays[sale['dia']] > allDays[busiest_day]:
+                busiest_day = sale['dia']
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        allDays = {}
+        busiest_day = self._data[0]['dia']
+
+        for sale in self._data:
+            if sale['dia'] not in allDays:
+                allDays[sale['dia']] = 1
+            else:
+                allDays[sale['dia']] += 1
+            if allDays[sale['dia']] < allDays[busiest_day]:
+                busiest_day = sale['dia']
+        return busiest_day
